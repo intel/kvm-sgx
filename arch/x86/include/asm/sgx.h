5bbba1361fcfc4b6dfac52b226f12ee08ee663d5
Sean Christopherson <sean.j.christopherson@intel.com>
2018-08-20 09:31:15 -0700

    x86/sgx: Use C-style comments for SPDX license
    Per Documentation/process/license-rules.rst, C style comments should
    be used for the SPDX license in C header files.  Older assembler tools
    cannot handle C++ style comments.
    
    Fixes: df9471602915 ("x86/sgx: Detect Intel SGX")
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>

--- arch/x86/include/asm/sgx.h
+++ arch/x86/include/asm/sgx.h
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: (GPL-2.0 OR BSD-3-Clause)
-// Copyright(c) 2016-18 Intel Corporation.
+/* SPDX-License-Identifier: (GPL-2.0 OR BSD-3-Clause) */
+/* Copyright(c) 2016-18 Intel Corporation. */
 
 #ifndef _ASM_X86_SGX_H
 #define _ASM_X86_SGX_H
