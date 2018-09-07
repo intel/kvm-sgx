602fe5d2595669fa4e72a8a81ddc57d703ec806f
Sean Christopherson <sean.j.christopherson@intel.com>
2018-09-07 12:20:29 -0700

    x86/sgx: add local header file (to expose functions to EPC cgroup)
    The EPC cgroup will need access to core SGX functions that should not
    be exported, e.g. sgx_reclaim_pages(), and the existence of the cgroup
    should be hidden from EPC consumers, e.g. the SGX driver and KVM.
    
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>

---
+++ arch/x86/kernel/cpu/intel_sgx.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/* Copyright(c) 2018 Intel Corporation. */
+
+#ifndef _ASM_X86_INTEL_SGX_H
+#define _ASM_X86_INTEL_SGX_H
+
+void sgx_reclaim_pages(void);
+
+#endif /* _ASM_X86_INTEL_SGX_H */
