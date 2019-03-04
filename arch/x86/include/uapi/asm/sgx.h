sean-jc/linux

sgx/x86: Add SGX_IOC_ENCLAVE_REMOVE_PAGES to the SGX driver
SGX_IOC_ENCLAVE_REMOVE_PAGES removes a range of pages by applying
ENCLS(EREMOVE). The use case for this is to dynamically remove pages
from an enclave by first changing their type to TRIM and then applying
this ioctl. Removing trimmed pages keeps the enclave in a valid state.

Signed-off-by: Jarkko Sakkinen <jarkko.sakkinen@linux.intel.com>

arch/x86/include/uapi/asm/sgx.h
@@ -16,6 +16,8 @@
 	_IOW(SGX_MAGIC, 0x01, struct sgx_enclave_add_page)
 #define SGX_IOC_ENCLAVE_INIT \
 	_IOW(SGX_MAGIC, 0x02, struct sgx_enclave_init)
+#define SGX_IOC_ENCLAVE_REMOVE_PAGES \
+	_IOW(SGX_MAGIC, 0x03, struct sgx_enclave_remove_pages)
 
 /* IOCTL return values */
 #define SGX_POWER_LOST_ENCLAVE		0x40000000
@@ -55,5 +57,16 @@ struct sgx_enclave_init {
 	__u64	addr;
 	__u64	sigstruct;
 };
+
+/**
+ * struct sgx_enclave_remove_pages - parameter structure for the
+ *                                   %SGX_IOC_ENCLAVE_REMOVE_PAGES ioctl
+ * @addr:	address in the ELRANGE for the first page
+ * @length:	length of the address range (must be multiple of the page size)
+ */
+struct sgx_enclave_remove_pages {
+	__u64	addr;
+	__u64	length;
+} __packed;
 
 #endif /* _UAPI_ASM_X86_SGX_H */

intel/kvm-sgx

arch/x86/include/uapi/asm/sgx.h
@@ -18,6 +18,8 @@
 	_IOW(SGX_MAGIC, 0x02, struct sgx_enclave_init)
 #define SGX_IOC_ENCLAVE_SET_ATTRIBUTE \
 	_IOW(SGX_MAGIC, 0x03, struct sgx_enclave_set_attribute)
+#define SGX_IOC_ENCLAVE_REMOVE_PAGES \
+	_IOW(SGX_MAGIC, 0x04, struct sgx_enclave_remove_pages)
 
 /* IOCTL return values */
 #define SGX_POWER_LOST_ENCLAVE		0x40000000
@@ -68,5 +70,16 @@ struct sgx_enclave_set_attribute {
 	__u64	addr;
 	__u64	attribute_fd;
 };
+
+/**
+ * struct sgx_enclave_remove_pages - parameter structure for the
+ *                                   %SGX_IOC_ENCLAVE_REMOVE_PAGES ioctl
+ * @addr:	address in the ELRANGE for the first page
+ * @length:	length of the address range (must be multiple of the page size)
+ */
+struct sgx_enclave_remove_pages {
+	__u64	addr;
+	__u64	length;
+} __packed;
 
 #endif /* _UAPI_ASM_X86_SGX_H */