ba22a087fb02512dbde6d1bb4c9c89b72fd8b9d0
Sean Christopherson <sean.j.christopherson@intel.com>
2019-12-11 13:11:45 -0800

    x86/sgx: Move ENCLS leaf definitions to sgx_arch.h
    Move the ENCLS leaf definitions to sgx_arch.h so that they can be used
    by KVM.  And because they're architectural.
    
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>

--- arch/x86/include/asm/sgx_arch.h
+++ arch/x86/include/asm/sgx_arch.h
@@ -14,6 +14,21 @@
 #define SGX_CPUID				0x12
 #define SGX_CPUID_FIRST_VARIABLE_SUB_LEAF	2
 
+enum sgx_encls_leaf {
+	ECREATE	= 0x00,
+	EADD	= 0x01,
+	EINIT	= 0x02,
+	EREMOVE	= 0x03,
+	EDGBRD	= 0x04,
+	EDGBWR	= 0x05,
+	EEXTEND	= 0x06,
+	ELDU	= 0x08,
+	EBLOCK	= 0x09,
+	EPA	= 0x0A,
+	EWB	= 0x0B,
+	ETRACK	= 0x0C,
+};
+
 /**
  * enum sgx_return_code - The return code type for ENCLS, ENCLU and ENCLV
  * %SGX_NOT_TRACKED:		Previous ETRACK's shootdown sequence has not
