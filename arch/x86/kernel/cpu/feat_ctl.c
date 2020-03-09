7f455b9bec917c08b1237e6c52bd7313c4cf09db
Sean Christopherson <sean.j.christopherson@intel.com>
2020-03-09 14:46:55 -0700

    x86/cpu/intel: Allow SGX virtualization without Launch Control support
    Allow SGX virtualization on systems without Launch Control support, i.e.
    allow KVM to expose SGX to guests that support non-LC configurations.
    
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>

--- arch/x86/kernel/cpu/feat_ctl.c
+++ arch/x86/kernel/cpu/feat_ctl.c
@@ -123,13 +123,16 @@ void init_ia32_feat_ctl(struct cpuinfo_x86 *c)
 	}
 
 	/*
-	 * Enable SGX if and only if the kernel supports SGX and Launch Control
-	 * is supported, i.e. disable SGX if the LE hash MSRs can't be written.
+	 * Enable SGX if and only if the kernel supports SGX.  Require Launch
+	 * Control support if SGX virtualization is *not* supported, i.e.
+	 * disable SGX if the LE hash MSRs can't be written and SGX can't be
+	 * exposed to a KVM guest (which might support non-LC configurations).
 	 */
 	enable_sgx = cpu_has(c, X86_FEATURE_SGX) &&
 		     cpu_has(c, X86_FEATURE_SGX1) &&
-		     cpu_has(c, X86_FEATURE_SGX_LC) &&
-		     IS_ENABLED(CONFIG_INTEL_SGX);
+		     IS_ENABLED(CONFIG_INTEL_SGX) &&
+		     (cpu_has(c, X86_FEATURE_SGX_LC) ||
+		      IS_ENABLED(CONFIG_INTEL_SGX_VIRTUALIZATION));
 
 	if (msr & FEAT_CTL_LOCKED)
 		goto update_caps;
@@ -152,8 +155,11 @@ void init_ia32_feat_ctl(struct cpuinfo_x86 *c)
 			msr |= FEAT_CTL_VMX_ENABLED_INSIDE_SMX;
 	}
 
-	if (enable_sgx)
-		msr |= FEAT_CTL_SGX_ENABLED | FEAT_CTL_SGX_LC_ENABLED;
+	if (enable_sgx) {
+		msr |= FEAT_CTL_SGX_ENABLED;
+		if (cpu_has(c, X86_FEATURE_SGX_LC))
+			msr |= FEAT_CTL_SGX_LC_ENABLED;
+	}
 
 	wrmsrl(MSR_IA32_FEAT_CTL, msr);
 
@@ -176,8 +182,9 @@ update_caps:
 	}
 
 update_sgx:
-	if (!(msr & FEAT_CTL_SGX_ENABLED) ||
-	    !(msr & FEAT_CTL_SGX_LC_ENABLED) || !enable_sgx) {
+	if (!(msr & FEAT_CTL_SGX_ENABLED) || !enable_sgx ||
+	   (!(msr & FEAT_CTL_SGX_LC_ENABLED) &&
+	    !IS_ENABLED(CONFIG_INTEL_SGX_VIRTUALIZATION))) {
 		if (enable_sgx)
 			pr_err_once("SGX disabled by BIOS\n");
 		clear_sgx_caps();
