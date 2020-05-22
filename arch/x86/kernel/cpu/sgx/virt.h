019906e55bdb9b85d98e792b8b54de6fef0b27da
Sean Christopherson <sean.j.christopherson@intel.com>
2020-05-22 00:26:01 -0700

    x86/sgx: Add EPC OOM support for virtual EPC
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>

--- arch/x86/kernel/cpu/sgx/virt.h
+++ arch/x86/kernel/cpu/sgx/virt.h
@@ -4,11 +4,19 @@
 
 #ifdef CONFIG_INTEL_SGX_VIRTUALIZATION
 int __init sgx_virt_epc_init(void);
+bool sgx_virt_epc_get_ref(struct sgx_epc_page *epc_page);
+void sgx_virt_epc_oom(struct sgx_epc_page *epc_page);
 #else
 static inline int __init sgx_virt_epc_init(void)
 {
 	return -ENODEV;
 }
+static inline bool sgx_virt_epc_get_ref(struct sgx_epc_page *epc_page)
+{
+	WARN_ON_ONCE(1);
+	return false;
+}
+static inline void sgx_virt_epc_oom(struct sgx_epc_page *epc_page) {}
 #endif
 
 #endif /* _ASM_X86_SGX_VIRT_H */
