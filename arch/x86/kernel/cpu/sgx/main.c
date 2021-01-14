65d0543f2699f323de69e3205689c5e382849fbd
Kai Huang <kai.huang@intel.com>
2021-01-14 23:54:40 +1300

    x86/sgx: Initialize virtual EPC driver even when SGX driver is disabled
    Modify sgx_init() to always try to initialize the virtual EPC driver,
    even if the SGX driver is disabled.  The SGX driver might be disabled
    if SGX Launch Control is in locked mode, or not supported in the
    hardware at all.  This allows (non-Linux) guests that support non-LC
    configurations to use SGX.
    
    Acked-by: Dave Hansen <dave.hansen@intel.com>
    Reviewed-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: Kai Huang <kai.huang@intel.com>

--- arch/x86/kernel/cpu/sgx/main.c
+++ arch/x86/kernel/cpu/sgx/main.c
@@ -712,7 +712,15 @@ static int __init sgx_init(void)
 		goto err_page_cache;
 	}
 
-	ret = sgx_drv_init();
+	/*
+	 * Always try to initialize the native *and* KVM drivers.
+	 * The KVM driver is less picky than the native one and
+	 * can function if the native one is not supported on the
+	 * current system or fails to initialize.
+	 *
+	 * Error out only if both fail to initialize.
+	 */
+	ret = !!sgx_drv_init() & !!sgx_vepc_init();
 	if (ret)
 		goto err_kthread;
 
